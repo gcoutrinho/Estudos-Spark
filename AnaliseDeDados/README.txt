2bc91c9833b236217518c4ecbf0033512287272d
Gean Alifi Coutrinho <gean.coutrinho@hotmail.com>
2019-10-02 01:41:27 -0300

    AJuste

--- AnaliseDeDados/README.txt
+++ AnaliseDeDados/README.txt
@@ -5,3 +5,6 @@ Para que seja possivel usar as funcionalidades do Spark use a seguinte dependenc
 	    <artifactId>spark-core_2.12</artifactId>
 	    <version>2.4.4</version>
 </dependency> 
+
+Será necessário alterar o caminho de onde se encontra os caminhos dos arquivos a serem utilizados.
+Local: Classe DesafioSpark linhas 21 e 22
--- AnaliseDeDados/src/main/java/DesafioSpark.java
+++ AnaliseDeDados/src/main/java/DesafioSpark.java
@@ -18,8 +18,8 @@ public class DesafioSpark {
          JavaSparkContext ctx = new JavaSparkContext(conf);
          
          //Carrega o conteúdo do TXT para dentro do RDD do Spark
-         JavaRDD<String> rddAugust = ctx.textFile("C:/Users/William/eclipse-workspace/AnaliseDeDados/access_log_Aug95");
-         JavaRDD<String> rddJuly = ctx.textFile("C:/Users/William/eclipse-workspace/AnaliseDeDados/access_log_Jul95");
+         JavaRDD<String> rddAugust = ctx.textFile("C:/access_log_Aug95");
+         JavaRDD<String> rddJuly = ctx.textFile("C:/access_log_Jul95");
          
          //Une os dois RDDs dentro de um unico
          JavaRDD<String> unionRDD = rddJuly.union(rddAugust);
@@ -43,7 +43,7 @@ public class DesafioSpark {
 	public static void retornaErros404PorDia(JavaRDD<String> unionRDD) {
 		JavaRDD<String> errosFiltrados = unionRDD.filter(x -> x.contains("404"));
 		JavaPairRDD<String, Integer> filtered = errosFiltrados
-				.mapToPair(m -> new Tuple2<String, Integer>(m.split("-.-\\w|\\[|\\]|\"|")[4].split(":")[4], 1));
+				.mapToPair(m -> new Tuple2<String, Integer>(m.split("-.-\\w|\\[|\\]|\"| ")[4].split(":")[0], 1));
 		
 		JavaPairRDD<String, Integer> agrupaErrosPorDia = filtered.reduceByKey((x, y) -> x + y);	
 		
@@ -62,7 +62,7 @@ public class DesafioSpark {
 		JavaRDD<String> filtraErros404 = unionRDD.filter(x -> x.contains("404"));
 		
 		JavaPairRDD<String, Integer> pairErros = filtraErros404.
-											mapToPair(x -> new Tuple2<String, Integer>(x.split("-.-\\w|\\[|\\]|\"|")[0], 1));
+											mapToPair(x -> new Tuple2<String, Integer>(x.split("-.-\\w|\\[|\\]|\"| ")[0], 1));
 		
 		//Agrupa e ordena os hosts top 5 erros
 		List<Tuple2<String, Integer>> sorteErros404 = pairErros.reduceByKey((x, y) -> x + y).sortByKey(true).take(5);
@@ -78,7 +78,7 @@ public class DesafioSpark {
 		//Usa o flatMap para semparar as strings
 		JavaRDD<Integer> dataFiltered = unionRDD.flatMap(t -> {
 			try {
-				return Arrays.asList(Integer.parseInt(t.split("-.-\\w|\\[|\\]|\"|")[13])).iterator();
+				return Arrays.asList(Integer.parseInt(t.split("-.-\\w|\\[|\\]|\"| ")[13])).iterator();
 
 			} catch (Exception e) {
 				return Arrays.asList(0).iterator();
@@ -93,7 +93,7 @@ public class DesafioSpark {
 	private static Integer retornaNumeroDeHostsUnicos(JavaRDD<String> unionRDD) {	
 	
 		 JavaPairRDD<String, Integer> host = unionRDD
-				    .mapToPair(s -> new Tuple2<String, Integer>(s.split("-.-\\w|\\[|\\]|\"|")[0], 1));
+				    .mapToPair(s -> new Tuple2<String, Integer>(s.split("-.-\\w|\\[|\\]|\"| ")[0], 1));
 				  JavaPairRDD<String, Integer> hostsAgrupados = host.reduceByKey((x, y) -> x + y);
 				  
 				  Integer totalHostsUnicos = (int) hostsAgrupados.filter(x -> x._2 == 1).count();
